cc0030db4669ca7bcbdff15421ad4260d7801c0a
j105rob <j105.rob@gmail.com>
2013-06-20 11:35:05 -0400

    Added output plugins

---
+++ collector/output/main.py
@@ -0,0 +1,21 @@
+'''
+Created on Nov 13, 2012
+
+@author: rob
+'''
+from collector.base import PlugableBase
+
+import utils.settings as Settings
+
+class Transform(PlugableBase):
+    def __init__(self):
+        self.stage = "output"
+        return super(Transform,self).__init__()
+
+    def run(self,inputObject):
+        #TODO: need a driver for the parsing routine based on the way that the netflow record is laid out
+        #    this should be a plugin that is pluggable to run the field level plugins
+        
+        for key in self.modInstances:
+            self.modInstances[key].run(inputObject)
+        return inputObject
---
+++ collector/output/plugins/mysql.py
@@ -0,0 +1,13 @@
+'''
+Created on Nov 28, 2012
+
+@author: benjamin
+'''
+import utils.settings as Settings
+from collector.base import PluginBase
+
+class MySql(PluginBase):
+    
+    def run(self,inputObject):
+        #do something with inputObject
+        pass
